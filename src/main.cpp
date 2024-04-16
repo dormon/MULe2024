813d9bced5be64f7fbfd9570285434b42d94d903
dormon <imilet@fit.vutbr.cz>
2024-04-16 13:38:16 +0200

    SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<SDL.h>
+
 
 int main(int argc,char*argv[]){
   std::cerr << "Hello World!" << std::endl;
+  SDL_CreateWindow("MULe",0,0,1024,748,0);
+  bool running = true;
+  while(running){ // MAIN LOOP
+    SDL_Event e;
+    while(SDL_PollEvent(&e)){ // EVENT LOOP
+      if(e.type == SDL_QUIT)running = false;
+    }
+
+  };
+
   return 0;
 }
