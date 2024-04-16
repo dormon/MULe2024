2e11a80f0234c5354413ef63c475d82352f5ffb7
dormon <imilet@fit.vutbr.cz>
2024-04-16 13:56:40 +0200

    2 triangles

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,31 @@ int main(int argc,char*argv[]){
 
   ge::gl::init();
 
+  auto vs = std::make_shared<Shader>(GL_VERTEX_SHADER,R".(
+  #version 330
+
+  void main(){
+    if(gl_VertexID == 0)gl_Position = vec4(0,0,0,1);
+    if(gl_VertexID == 1)gl_Position = vec4(1,0,0,1);
+    if(gl_VertexID == 2)gl_Position = vec4(0,1,0,1);
+    if(gl_VertexID == 3)gl_Position = vec4(1,1,0,1);
+
+  }
+
+  ).");
+
+  auto fs = std::make_shared<Shader>(GL_FRAGMENT_SHADER,R".(
+  #version 330
+
+  out vec4 fColor;
+  void main(){
+    fColor = vec4(1);
+  }
+
+  ).");
+
+  auto prg = std::make_shared<Program>(vs,fs);
+
   bool running = true;
   while(running){ // MAIN LOOP
     SDL_Event e;
@@ -23,6 +48,8 @@ int main(int argc,char*argv[]){
     glClearColor(1,1,0,1);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    prg->use();
+    glDrawArrays(GL_TRIANGLE_STRIP,0,4);
 
     SDL_GL_SwapWindow(window);
   };
